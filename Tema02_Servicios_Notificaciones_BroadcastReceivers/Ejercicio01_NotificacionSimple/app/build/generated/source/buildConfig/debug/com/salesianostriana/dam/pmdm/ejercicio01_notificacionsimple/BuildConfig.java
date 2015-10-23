06b0b71db264e27cdfc70c5b5aa9c4ecaf2aa6f8
jesuspa17 <jpallares94.jp@gmail.com>
2015-10-23 08:22:59 +0200

    music service

---
+++ Tema02_Servicios_Notificaciones_BroadcastReceivers/Ejercicio01_NotificacionSimple/app/build/generated/source/buildConfig/debug/com/salesianostriana/dam/pmdm/ejercicio01_notificacionsimple/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.salesianostriana.dam.pmdm.ejercicio01_notificacionsimple;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.salesianostriana.dam.pmdm.ejercicio01_notificacionsimple";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
