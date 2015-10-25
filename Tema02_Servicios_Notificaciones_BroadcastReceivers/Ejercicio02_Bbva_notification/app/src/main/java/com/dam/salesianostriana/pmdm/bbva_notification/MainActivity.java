7792c6a31c8191aeef4cab97f94986adf91665ee
Jesús Pallares Suárez <jpallares94.jp@gmail.com>
2015-10-25 22:26:53 +0100

    Update MainActivity.java

--- Tema02_Servicios_Notificaciones_BroadcastReceivers/Ejercicio02_Bbva_notification/app/src/main/java/com/dam/salesianostriana/pmdm/bbva_notification/MainActivity.java
+++ Tema02_Servicios_Notificaciones_BroadcastReceivers/Ejercicio02_Bbva_notification/app/src/main/java/com/dam/salesianostriana/pmdm/bbva_notification/MainActivity.java
@@ -4,6 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+/**
+
+BBVA sms confirmación
+
+El BBVA en su app Android tiene un sistema de verifación mediante SMS para confirmar que el titular 
+está realizando la operación desde el teléfono móvil que está vinculado a su cuenta.
+
+Se pide implementar la pantalla en la que se recibe dicho código SMS. El proceso es el siguiente:
+
+    El usuario solicita realizar una transferencia.
+    El dispositivo recibe un SMS, mientras que la app está aguardando la recepción de dicho SMS.
+    Se lee el SMS recibido y si el remitente es 22100 que es la centralita de BBVA, se extrae de dicho SMS 
+    el código de verificación.
+    Dicho código extraído deberá ser mostrado en una notificación.
+    El usuario pulsa en el botón Aceptar para completar la operación. Se muestra un Toast indicando 
+    que la operación ha sido correcta.
+
+Ampliación:
+Dicho código extraído deberá ser introducido de manera automática por la app en el EditText en el que el usuario lo debería haber escrito, así ahorramos al usuario tener que escribirlo.
+ **/
 
 public class MainActivity extends AppCompatActivity {
 
