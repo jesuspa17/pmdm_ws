f5e1b1aec4a428ccd050915de73f8ae1e61e1685
Jesús Pallares Suárez <jpallares94.jp@gmail.com>
2016-01-10 23:22:26 +0100

    mapa

---
+++ PrisionBreak/app/src/main/java/com/dam/salesianostriana/prisionbreak/MapsActivity.java
@@ -0,0 +1,199 @@
+package com.dam.salesianostriana.prisionbreak;
+
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient = null;
+    private Location mCurrentLocation;
+    private boolean mRequestingLocationUpdates = true;
+    private LocationRequest mLocationRequest;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        //Instancia un objeto de tipo GoogleApiClient
+        instanciarGoogleApiClient();
+
+        //Activa la detección de localización
+        detectarLocalizacion();
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+       /* mMap = googleMap;
+        //Se añaden opciones al mapa.
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        //Aquí se crea el polígono que rodea al colegio.
+        PolygonOptions rectOptions = new PolygonOptions()
+                .add(new LatLng(37.379608,-6.006966),
+                        new LatLng(37.380512,-6.005775),
+                        new LatLng(37.381543,-6.00688),
+                        new LatLng(37.381151,-6.00747),
+                        new LatLng(37.381279,-6.00777),
+                        new LatLng(37.380946,-6.008392),
+                        new LatLng(37.379608,-6.006966))
+                .strokeColor(Color.RED)
+                .strokeWidth(15)
+                .fillColor(Color.BLUE);
+
+
+        Polygon polygon = mMap.addPolygon(rectOptions);
+        polygon.setVisible(true);*/
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        actualizarInterfaz();
+
+    }
+
+
+    private void actualizarInterfaz() {
+
+        if (mMap != null) {
+            mMap.clear();
+        }
+        LatLng posicion_actual = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(posicion_actual, 13));
+
+        final LatLng salesianos = new LatLng(37.379608,-6.006966);
+        PolygonOptions rectOptions = new PolygonOptions()
+                .add(new LatLng(37.379608,-6.006966),
+                        new LatLng(37.380512,-6.005775),
+                        new LatLng(37.381543,-6.00688),
+                        new LatLng(37.381151,-6.00747),
+                        new LatLng(37.381279,-6.00777),
+                        new LatLng(37.380946,-6.008392),
+                        new LatLng(37.379608,-6.006966))
+                .strokeColor(Color.BLUE)
+                .strokeWidth(15);
+
+        Polygon polygon = mMap.addPolygon(rectOptions);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(salesianos,15));
+
+
+    }
+
+    protected synchronized void instanciarGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                        // Indico que la API que voy a utilizar
+                        // dentro de Google Play Services, es la API
+                        // del Servicio de Localización
+                .addApi(LocationServices.API)
+                .build();
+
+    }
+
+    protected void detectarLocalizacion() {
+        mLocationRequest = new LocationRequest();
+        // Intervalo de uso normal de la la aplicación
+        mLocationRequest.setInterval(10000);
+        // Interval de una app que requiera una localización exhaustiva
+        mLocationRequest.setFastestInterval(30000);
+        // GPS > mejor método de localización / consume más batería
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    protected void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+        mRequestingLocationUpdates = true;
+        Log.i("POSICION", "POSICION: startLocationUpdates");
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+        mRequestingLocationUpdates = false;
+        Log.i("POSICION", "POSICION: stopLocationUpdates");
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i("POSICION", "POSICION: onConnected");
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected() && mRequestingLocationUpdates) {
+            stopLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected() && !mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {}
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {}
+}
+
