ffad5ac76d8ad09eba6210c588cc296f38c04e72
jesuspa17 <jpallares94.jp@gmail.com>
2016-02-01 14:06:40 +0100

    PriebaSonIDO

---
+++ Tema04_Sonidos_Sensores/Ejemplo01_Sonidos/app/src/main/java/com/dam/salesianostriana/ejemplo01_sonidos/MainActivity.java
@@ -0,0 +1,83 @@
+package com.dam.salesianostriana.ejemplo01_sonidos;
+
+import android.content.Context;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_play;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        btn_play = (Button) findViewById(R.id.btn_play);
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new TestSound(MainActivity.this, 0f, 1.0f);
+            }
+        });
+
+    }
+
+    public class TestSound {
+
+        private SoundPool soundPool;
+        private int id;
+
+        public TestSound(Context context,final float leftVolume, final float rightVolume) {
+            AudioAttributes aa = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+
+            soundPool = new SoundPool.Builder()
+                    .setMaxStreams(10)
+                    .setAudioAttributes(aa)
+                    .build();
+
+            id = soundPool.load(context, R.raw.helicoptero, 1);
+
+            soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+
+                public void onLoadComplete(SoundPool arg0, int arg1, int arg2) {
+                    soundPool.play(id, leftVolume, rightVolume, 1, 0, 1f);
+                };
+            });
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
